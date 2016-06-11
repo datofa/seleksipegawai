1c9f2a3ef7ae1ce7ae27d5a612972ad3a8f00e76
Zonierachma <rahmawatinurulzuni@gmail.com>
2016-06-11 10:56:56 +0700

    upload

---
+++ src/seleksipegawai/DbUtils.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seleksipegawai;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author zuni
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModel(ResultSet sql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
